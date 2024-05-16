6841536237b450e8a1481e3a4825b584ce9f7820
idohyeon <ehgus5026@naver.com>
2024-05-16 23:31:33 +0900

    -Item Controller

--- src/main/java/com/example/itemservice/web/basic/BasicItemController.java
+++ src/main/java/com/example/itemservice/web/basic/BasicItemController.java
@@ -8,9 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +33,43 @@ public class BasicItemController {
         model.addAttribute("item", findItem);
 
         log.info("itemI={}", itemId);
+
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String saveItemForm() {
+        return "basic/addForm";
+    }
+
+    /*@PostMapping("/add")
+    public String saveItem(@RequestParam String itemName,   // html form data가 그대로 쿼리 파라미터로 넘어옴
+                           @RequestParam Integer price,
+                           @RequestParam Integer quantity,
+                           Model model) {
+
+        Item newItem = new Item(itemName, price, quantity);
+        itemRepository.save(newItem);
+
+        model.addAttribute("item", newItem);
+
+        return "basic/item";
+    }*/
+
+    /**
+     * @ModelAttribute는 요청 파라미터의 객체도 만들어주고, View에도 ("")로 지정한 이름으로 모델 객체를 넣어줌
+     */
+    @PostMapping("/add")
+    public String saveItem(@ModelAttribute("item") Item item, Model model) {
+//        Item newItem = new Item(item);
+
+//        newItem.setItemName(item.getItemName());
+//        newItem.setPrice(item.getPrice());
+//        newItem.setQuantity(item.getQuantity());
+
+        itemRepository.save(item);
+
+//        model.addAttribute("item", newItem);
 
         return "basic/item";
     }
