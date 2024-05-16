f1e1a5e9f1195a0c3557d6061cdd5a8423138e51
idohyeon <ehgus5026@naver.com>
2024-05-16 22:45:56 +0900

    -View Controller

---
+++ src/main/java/com/example/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,51 @@
+package com.example.itemservice.web.basic;
+
+
+import com.example.itemservice.domain.Item;
+import com.example.itemservice.domain.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/basic/items") //기본 경로
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String findItem(@PathVariable Long itemId, Model model) {
+        Item findItem = itemRepository.findById(itemId);
+        model.addAttribute("item", findItem);
+
+        log.info("itemI={}", itemId);
+
+        return "basic/item";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 25000, 5));
+    }
+
+}
